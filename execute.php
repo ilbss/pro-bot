9a2a5fef41ed90d3382f90770690e15eab0eb58c
ilbss <85892288+ilbss@users.noreply.github.com>
2021-06-14 22:17:16 +0200

    Update execute.php

--- execute.php
+++ execute.php
@@ -49,11 +49,8 @@
 		$parameters = array('chat_id' => $chatId, "text" => $text);
 		
 	if($text == "Actis"){
-   		$Actis[0] ="Ma questa classe è un Bivacco!";
-   		$Actis[1] ="Fernicola, credi che noi insegnanti siamo dei celebrolesi?!";
-		$Actis[2] ="Il vostro libro è prolisso";
-		$i = rand(0,2);
-		$parameters = array('chat_id' => $chatId, "text" => $Actis[$i]);
+   		$text ="Ma questa classe è un Bivacco!";
+		$parameters = array('chat_id' => $chatId, "text" => $text);
   	}
 	
 
